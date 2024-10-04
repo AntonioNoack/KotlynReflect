45ef48d34c57fdd9168e59f0d90e5b19ed6e4144
Antonio Noack <antonio-noack@gmx.de>
2024-10-04 20:34:08 +0200

    Added missing interface

--- src/kotlyn/jvm/internal/PropertyReference1Impl.java
+++ src/kotlyn/jvm/internal/PropertyReference1Impl.java
@@ -1,11 +1,12 @@
 package kotlyn.jvm.internal;
 
+import kotlyn.reflect.KFunction;
 import kotlyn.reflect.KProperty1;
 
 @SuppressWarnings({"unused", "rawtypes"})
 public class PropertyReference1Impl<K, V>
         extends PropertyReference0Impl<K, V>
-        implements KProperty1<K, V> {
+        implements KProperty1<K, V>, KFunction<V> {
     public PropertyReference1Impl(Class clazz, String propertyName, String getterName, int idk) {
         super(clazz, propertyName, getterName, idk);
     }
